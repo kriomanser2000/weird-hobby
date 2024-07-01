c51f4cd2ef39335ad2898dca4d081717ad03b11a
Kriomanser2000 <kuzmenkasana05@gmail.com>
2024-07-01 18:31:26 +0300

    done

--- src/app/hobby/hobby.component.ts
+++ src/app/hobby/hobby.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-hobby',
-  standalone: true,
-  imports: [],
   templateUrl: './hobby.component.html',
-  styleUrl: './hobby.component.css'
+  styleUrls: ['./hobby.component.css']
 })
-export class HobbyComponent {
-
+export class HobbyComponent 
+{
+  hobbyName: string = "Дивне хобі";
+  hobbyDescription: string = "Цікаве хобі, яке підвласне не кожному.";
 }
